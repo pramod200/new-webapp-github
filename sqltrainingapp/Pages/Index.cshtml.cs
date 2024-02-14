8141873f2e0be4ed8bfffe90c06242280976508e
Administrator <Administrator@LAPTOP-PNOUBC84>
2024-02-14 00:38:59 -0500

    Add project files.

---
+++ sqltrainingapp/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqltrainingapp.Models;
+using sqltrainingapp.Services;
+
+namespace sqltrainingapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
